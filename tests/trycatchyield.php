b5f06a0d911b42583a0d04bf759eafdd0d689979
abluchet <abluchet@ds-restauration.com>
2018-04-03 18:08:07 +0200

    Reproduce

---
+++ tests/trycatchyield.php
@@ -0,0 +1,104 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\Asynchronous\Middleware\SendMessageMiddleware;
+use Symfony\Component\Messenger\Asynchronous\Routing\SenderLocator;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Messenger\Transport\Serialization\Serializer as MessageSerializer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Messenger\Worker;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
+use Symfony\Component\Messenger\HandlerLocator;
+use Symfony\Component\Messenger\Transport\SenderInterface;
+use Symfony\Component\Messenger\Transport\ReceiverInterface;
+use Symfony\Component\Messenger\Transport\Serialization\EncoderInterface;
+use Symfony\Component\Messenger\Transport\Serialization\DecoderInterface;
+
+// Build a serializer
+$encoders = array(new JsonEncoder());
+$normalizers = array(new ObjectNormalizer());
+$serializer = new Serializer($normalizers, $encoders);
+// Messenger encoder/decoder
+$messageSerializer = new MessageSerializer($serializer);
+
+class Message {
+    public $str;
+    public function __construct($str) {
+        $this->str = $str;
+    }
+}
+
+class Sender implements SenderInterface {
+    private $encoder;
+    public $list = [];
+
+    public function __construct(EncoderInterface $encoder) {
+        $this->encoder = $encoder;
+    }
+
+    public function send($message) {
+        $this->list[] = $this->encoder->encode($message);
+    }
+}
+
+class Receiver implements ReceiverInterface {
+    private $decoder;
+    private $sender;
+
+    public function __construct(DecoderInterface $decoder, SenderInterface $sender) {
+        $this->decoder = $decoder;
+        $this->sender = $sender;
+    }
+
+    public function receive(): iterable
+    {
+        while (true) {
+            if (null === $message = array_shift($this->sender->list)) {
+                continue;
+            }
+
+            try {
+                yield $this->decoder->decode($message);
+            } catch (\Throwable $e) {
+                echo 'Got exception do nothing'.PHP_EOL;
+            }
+        }
+    }
+}
+
+$sender = new Sender($messageSerializer);
+$receiver = new Receiver($messageSerializer, $sender);
+
+$container = new Container();
+$container->set('sender', $sender);
+
+$throwed = false;
+
+$handler = function ($t) use (&$throwed) {
+    if (false === $throwed) {
+        $throwed = true;
+        throw new \Exception('Fail');
+    }
+
+    echo sprintf('Got message %s', $t->str).PHP_EOL;
+};
+
+$bus = new MessageBus(array(
+    new SendMessageMiddleware(new SenderLocator($container, array(
+        Message::class => array('sender'),
+    ))),
+    new HandleMessageMiddleware(new HandlerLocator(array(
+        Message::class => $handler,
+    ))),
+));
+
+$bus->dispatch(new Message('test1'));
+$bus->dispatch(new Message('test2'));
+$bus->dispatch(new Message('test3'));
+
+$worker = new Worker($receiver, $bus);
+$worker->run();
